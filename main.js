7c90d6f5672a61e6cecb4b96116b4426dd8fa765
maaroufMed <60034486+maaroufMed@users.noreply.github.com>
2020-04-29 13:45:34 +0000

    todo

--- main.js
+++ main.js
@@ -2,9 +2,14 @@
 let theInput = document.querySelector(".add-item input");
 let list = document.getElementById("list");
 let noTask = document.querySelector(".todo-content .no-task");
-let addItem = document.querySelector('.add-item i');
+let addItem = document.querySelector('.add-item .add');
+let clear = document.querySelector('.add-item .clear');
+
+let task = document.getElementById('elzero-task');
 
 
+
+let itemsArray = [];
 
 
 
@@ -32,48 +37,49 @@ function addToDo(todo) {
 theInput.onkeyup = function (e) {
 	if (e.keyCode === 13) {
 		let todo = theInput.value;
-		if (theInput.value === "" ) {
-			swal('sory this input field is empty');
+		if (theInput.value.trim() === "") {
+			swal('لم تقم بادخال مهمة');
 		} else {
-			
-		test = [];
-		test.push(todo);
-		console.log(test);
-			
-		addToDo(todo);
-		noTask.remove();
-		theInput.value = "";
-			
-		console.log(test);	
+			let duplicate = 0;
+			itemsArray.forEach(item => { if (item === theInput.value) { duplicate = 1 } })
+			if (duplicate) { swal("لقد تم ادخال هذه المهة مسبقا") } else {
 
-		test.forEach(function() {
-			if (theInput.value === test.textContent) {
-				console.log('yes');
+				addToDo(todo);
+				itemsArray.push(todo)
+
+				noTask.remove();
+				theInput.value = "";
 			}
-		});
-
 		}
+
 	}
-
 }
-
 // add item with click on btn
 addItem.onclick = function () {
 	let todo = theInput.value;
-	if (theInput.value === "" ) {
-		swal('sory this input field is empty');
+	if (theInput.value.trim() === "") {
+		swal('لم تقم بادخال مهمة');
 	} else {
-		test = [];
-		test.push(todo);
-		console.log(test);
-		addToDo(todo);
-		noTask.remove();
-		theInput.value = "";
-		theInput.focus();
+		let duplicate = 0;
+		itemsArray.forEach(item => { if (item === theInput.value) { duplicate = 1 } })
+		if (duplicate) { swal("لقد تم ادخال هذه المهة مسبقا") } else {
+
+
+			addToDo(todo);
+			itemsArray.push(todo)
+
+			noTask.remove();
+			theInput.value = "";
+			theInput.focus();
+		}
+
 	}
+
+
 }
 
-document.addEventListener('click', function(e) {
+
+document.addEventListener('click', function (e) {
 	// check in item 
 	if (e.target.classList.contains('co')) {
 		// toggle class of icon
@@ -83,20 +89,33 @@ document.addEventListener('click', function(e) {
 		e.target.style.color = "green";
 		e.target.nextElementSibling.style.color = '#999';
 
-	} 
-	
+	}
+
 	// return default style
 	if (e.target.classList.contains("fa-circle")) {
 		e.target.style.color = "#000";
 		e.target.nextElementSibling.style.color = "#000";
 	}
-	
 
-	
+
+
 	// delete item
-	if (e.target.classList.contains("delete")){
+	if (e.target.classList.contains("delete")) {
 		e.target.parentNode.remove();
 	}
-	
+
 });
 
+// remove the all item 
+clear.onclick = function () {
+	list.innerHTML = "";
+}
+
+taskElzero = "Hello We Are Alzero Aeb Achool, We Love Our Members And This Is Our Js New Task";
+
+let firstWord = taskElzero.slice(0,5);
+let lastWord = taskElzero.slice(-5,);
+let nOfCar = taskElzero.length;
+let nOfWord = taskElzero.split(' ').length;
+console.log(nOfCar);
+console.log(nOfWord);
